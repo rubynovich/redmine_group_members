2ca42f98e17cc967fb88e2e8124ccc41a0f6276e
rubynovich <roman@shipiev.me>
2013-04-12 22:48:00 +0400

    fix: copy-past error

--- lib/redmine_group_members/hooks.rb
+++ lib/redmine_group_members/hooks.rb
@@ -1,4 +1,4 @@
-module RedmineApprovalPage
+module RedmineGroupMembers
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_issues_sidebar_planning_bottom,
               :partial => 'issues/group_members'
